3c47e26d7673e802f7125b7fcf78e91c392aa2e3
Panayot Marinov <panayot.marinov12@gmail.com>
2017-10-28 12:13:28 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="css/MainStyle.css"> 
+<link rel="import" href="import/doc.html">
+
+<title>Natural Medicine</title>
+<meta charset = "UTF-8"/>
+<script src="https://www.w3schools.com/lib/w3.js"></script>
+</head>
+
+<body>
+<div w3-include-html="https://www.w3schools.com/lib/h1.html"></div> 
+<div w3-include-html="register.html"></div> 
+<script>
+w3.includeHTML();
+</script>
+<section>
+<button type="button" class="btn btn-primary">Primary</button>
+</section>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+<link rel="stylesheet" type="text/css" href="css/MainStyle.css"> 
+<title>Natural Medicine</title>
+<meta charset = "UTF-8"/>
+</head>
+<body>
+<?php include("navigation.html"); ?>
+<section>
+<button type="button" class="btn btn-primary">Primary</button>
+</section>
+</body>
+</html>
